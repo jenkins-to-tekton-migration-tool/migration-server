010feedae328df4df3a3b9f77f16ac0c77d6869e
Hemant Kumar <hemantkumarcpersonal@gmail.com>
2024-10-19 18:11:35 +0530

    Refactor /mappings API to accept input from request body

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const jsonData = fs.readFileSync('tekton_tasks.data', 'utf-8');
 const dataObject = JSON.parse(jsonData);
 const Fuse = require('fuse.js');
 
+app.use(express.json());
+
 const fuseOptions = {
   isCaseSensitive: true,
   includeScore: true,
@@ -28,24 +30,43 @@ const fuseOptions = {
 
 const fuse = new Fuse(dataObject.data, fuseOptions);
 
+var mappings_data = {}
 
 app.get('/', function(req, res) {
   res.send('Jenkins -> Tekton HTTP Server is working!')
 });
 
-app.get('/tektontasks/data', function(req, res) {
-  res.json(dataObject);
+app.get('/tasks/data', function(req, res) {
+  res.status(200).json(dataObject);
   console.log('Tekton Tasks successfully retrieved!');
 });
 
+// I am assuming that this is how the request body is sent to the /mappings API endpoint
+//{
+//  "plugins": ["git", "prometheus", "mailer"]
+//}
 app.post('/mappings', function(req, res) {
 
-  var searchPattern = "builah"
-  console.log(fuse.search(searchPattern, { limit: 5}))
-  res.status(200).send('Mappings created !')
+  let plugins = req.body.plugins
+  // console.log(plugins)
+
+  for(let i = 0; i < plugins.length; i++)  
+  {
+    let searchPattern = plugins[i]
+
+    let search_results = fuse.search(searchPattern, { limit: 5}).map((element) => ({
+      task: element.item.name,
+      score: element.score
+    }))
+    console.log(search_results)
+
+    var task_names = search_results.map((element) => `${element.task}`)
+    mappings_data[plugins[i]] = task_names
+  }
+
+  res.status(200).json(mappings_data)
 
 })
-
 
 app.use(function(req, res, next) {
   res.status(404).send('Sorry, that route doesnt exist yet!')
